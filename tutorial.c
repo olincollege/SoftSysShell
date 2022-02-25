efe5acac40db6d12053b9d2afc820c113e37b1aa
Simrun Mutha <simrun.mutha@gmail.com>
2022-02-25 13:17:58 -0500

    idk

--- tutorial.c
+++ tutorial.c
@@ -1,43 +1,85 @@
 /* Following tutorial at this link:
-https://indradhanush.github.io/blog/writing-a-unix-shell-part-1/*/
+https://indradhanush.github.io/blog/writing-a-unix-shell-part-1/ */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <wait.h>
 
+int **get_input(char *input){
+    // Seperating the input string by space to return an array of strings
+    char **command = malloc(8 * sizeof(char *)); //double asterisk is a double pointer(pointer to a pointer)
+    char *delimeter = " ";
+    char *parsed;
+    int i = 0;
+    // using strtok to seperate string by space
+    parsed = strtok(input, delimeter);
+    while (parsed != NULL) {
+        command[i] = parsed;
+        i++;
+
+        parsed = strtok(NULL, delimeter);
+    }
+    command[i] = NULL;
+    return command;
+}
+
+int cd(char *path){
+    return chdir(path);
+}
+
 int main(){
 
     // fork system returns twice: a parent and a child process
     // you cannot control which process will run first by default
 
-    pid_t child_pid= fork(); // pid_t datatype represents process IDs
+    pid_t child_pid; // pid_t datatype represents process IDs
     pid_t wait_result;
     int status_location;
+    char **input;
+    char **command;
 
     // The child PID is 0 for one of the cases because
     // it is calling the getpid on the child of the child which doesnt exist
 
-    // everything under 'if' is the child process
-    if (child_pid == 0){
-        printf("CHILD Current PID: %d and Child PID: %d\n",
+    while(1){
+
+        input = readline("shell> ");
+        command = get_input(input);
+
+        if (strcmp("cd", command[0]) == 0){
+            cd(command[1]);
+            continue;
+        }
+
+        child_pid= fork(); 
+        // everything under 'if' is the child process
+        if (child_pid == 0){
+            // execvp executes a command where the first argument is the 
+            // command and the seconnd arguement is the command plus all the optioons
+            // it always ends with a null character
+            execvp(command[0], command); 
+            printf("CHILD Current PID: %d and Child PID: %d\n",
+                    getpid(), child_pid);
+            sleep(1);
+        }
+
+        // everything under 'else' is the parent process
+        else{
+            // waitpid documentation here: https://linux.die.net/man/2/waitpid
+            // waits until child process is over to execute this.
+            wait_result = waitpid(child_pid, &status_location, 0);
+
+            printf("PARENT Current PID: %d and Child PID: %d\n",
                 getpid(), child_pid);
-        sleep(1);
-    }
-
-    // everything under 'else' is the parent process
-    else{
-        // waitpid documentation here: https://linux.die.net/man/2/waitpid
-        // waits until child process is over to execute this.
-        wait_result = waitpid(child_pid, &status_location, 0);
-
-        printf("PARENT Current PID: %d and Child PID: %d\n",
-               getpid(), child_pid);
-        // Prints the results from waitpid.
-        printf("Results of waitpid:\nReturn- %d, Status - %i \n", 
-               wait_result, status_location);
-        
-
+            // Prints the results from waitpid.
+            printf("Results of waitpid:\nReturn- %d, Status - %i \n", 
+                wait_result, status_location);         
+        }
+        free(input);
+        free(command);
     }
     return 0;
 }
+
+// Compile using gcc -g -lreadline shell.c
